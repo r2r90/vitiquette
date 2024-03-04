2746945144c8d60a4b35851b7ac787e506b737e6
Artur Agh <aghartur@gmail.com>
2024-03-04 19:35:37 +0100

    import error resolved webpack & Canvas dynamic error added

--- src/components/editor-section/Canvas.tsx
+++ src/components/editor-section/Canvas.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Stage} from "react-konva";
+import {Layer, Rect, Stage, Text} from "react-konva";
 
 const Canvas = () => {
     return (
         <div>
-            <Stage>
+            <Stage width={200} height={200}>
+                <Layer>
 
+                </Layer>
             </Stage>
         </div>
     )
